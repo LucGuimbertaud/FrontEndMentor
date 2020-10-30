10cf084ed76d56b4f5ce68332fa9342c433e942d
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-10-30 14:48:24 +0100

    add some animation

--- faq-accordion-card-main/script/display_text.js
+++ faq-accordion-card-main/script/display_text.js
@@ -1,13 +1,20 @@
 function displayAnswer(id) {
   let answers = document.getElementsByClassName("faq_answer");
+  let questions = document.getElementsByClassName("faq_question");
 
   if (answers[id].style.position == "relative") {
+    answers[id].style.display = "none"
     answers[id].style.position = "absolute";
-    answers[id].style.transition = "opacity 0s"
+    questions[id].style.color = "hsl(240, 6%, 50%)"
+    questions[id].style.fontWeight = "400";
+    answers[id].style.transition = "opacity 0s";
     answers[id].style.opacity = "0";
   } else {
+    answers[id].style.display = "block"
     answers[id].style.position = "relative";
-    answers[id].style.transition = "opacity 1s"
+    answers[id].style.transition = "opacity 1s";
+    questions[id].style.fontWeight = "800";
+    questions[id].style.color = "hsl(237, 12%, 33%)"
     answers[id].style.opacity = "1";
   }
 }
