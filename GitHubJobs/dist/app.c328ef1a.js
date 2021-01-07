107edda07e3a3320caf72fc82958be86185c4020
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2021-01-07 10:26:35 +0100

    Add footer. Connect the search bar

--- GitHubJobs/dist/app.c328ef1a.js
+++ GitHubJobs/dist/app.c328ef1a.js
@@ -10121,7 +10121,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54022" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65066" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
